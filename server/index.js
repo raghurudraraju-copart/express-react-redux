b7b3fe8857e5db6442f698e8cebf28cd31f2bf4b
Raghu Rudraraju <Raghu.Rudraraju@copart.com>
2018-02-22 17:42:52 +0530

    added basic html file

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import path from 'path';
 
 let app = express();
 
 app.get('/*', (req,res) => {
-  res.send('Hello World');
+  //res.send('Hello World');
+  res.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.listen(1234, () => console.log('Application is running on localhost:1234'));
